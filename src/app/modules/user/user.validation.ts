8d5d1db498b544ee075188bec0943bc3af8347b5
Foysal79 <onthor312001@gmail.com>
2026-01-14 01:35:54 +0600

    add zod schemas for user authentication and role management

---
+++ src/app/modules/user/user.validation.ts
@@ -0,0 +1,25 @@
+import z from "zod";
+
+const registerSchema = z.object({
+    name : z.string().min(3),
+    email : z.string().email().email("Please provide a valid email address."),
+    password : z.string().min(6),
+})
+
+const loginSchema = z.object({
+  email: z.string().email(),
+  password: z.string().min(6),
+});
+
+const updateRoleSchema = z.object({
+  userId: z.string().length(24),
+  role: z.enum(['Admin', 'User']),
+});
+
+
+
+export const userValidation = {
+    registerSchema,
+    loginSchema,
+    updateRoleSchema
+}
