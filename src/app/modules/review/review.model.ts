41a09cbbd24ea3d3d3b3d9d6847f7ca7e1521be8
Foysal79 <onthor312001@gmail.com>
2026-01-14 20:10:43 +0600

    review model create

---
+++ src/app/modules/review/review.model.ts
@@ -0,0 +1,16 @@
+import { Schema, model } from "mongoose";
+import { IReview } from "./review.interface";
+
+const reviewSchema = new Schema<IReview>(
+  {
+    user: { type: Schema.Types.ObjectId, ref: "User", required: true },
+    book: { type: Schema.Types.ObjectId, ref: "Book", required: true },
+    rating: { type: Number, required: true, min: 1, max: 5 },
+    comment: String,
+    status: { type: String, enum: ["pending", "approved"], default: "pending" },
+    isDeleted: { type: Boolean, default: false },
+  },
+  { timestamps: true }
+);
+
+export const Review = model<IReview>("Review", reviewSchema);
