5b88650d264213721bce854ac5c95271d2e63495
Foysal79 <onthor312001@gmail.com>
2026-01-15 22:30:59 +0600

    reaging goal progress interface create

---
+++ src/app/modules/readingGoal/readingGoal.interface.ts
@@ -0,0 +1,23 @@
+import { Types } from "mongoose";
+
+export type GoalPeriod = "weekly" | "monthly" | "yearly";
+
+export type IReadingGoal = {
+    user : Types.ObjectId;
+    period : GoalPeriod;
+
+    targetBook : number;
+
+    startDate : string;
+    endDate : string;
+
+    isActive : boolean;
+    isDeleted : boolean;
+}
+
+export type IReadingGoalProgress = {
+    goal : IReadingGoal;
+    completedBooks : number;
+    remaining : number;
+    percentage : number;
+}
