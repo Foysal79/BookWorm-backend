f587dc3013443bfeac9fb798dcd3893f0a487fb1
Foysal79 <onthor312001@gmail.com>
2026-01-15 22:52:02 +0600

    reaging goal model create

---
+++ src/app/modules/readingGoal/readingGoal.model.ts
@@ -0,0 +1,33 @@
+import { model, Schema } from "mongoose";
+import { IReadingGoal } from "./readingGoal.interface";
+import { string } from "zod";
+
+const schema = new Schema<IReadingGoal>(
+  {
+    user: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    period: {
+      type: String,
+      enum: ["weekly", "monthly", "yearly"],
+      required: true,
+    },
+    targetBook: {
+      type: Number,
+      required: true,
+      min: 1,
+    },
+    startDate: { type: String, required: true },
+    endDate: { type: String, required: true },
+
+    isActive: { type: Boolean, default: true },
+    isDeleted: { type: Boolean, default: false },
+  },
+  {
+    timestamps: true,
+  }
+);
+schema.index({ user: 1, isActive: 1 });
+export const ReadingFoal = model<IReadingGoal>("ReadingGoal", schema);
